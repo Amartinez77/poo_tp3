209920cff2257426b1f50b0dfe480d626e828c6a
Ariel Martinez <ariel.mtz85@gmail.com>
2024-09-04 17:46:35 -0300

    Corrigiendo bugs

--- tallerTp03/src/ar/edu/unju/fi/util/FechaUtil.java
+++ tallerTp03/src/ar/edu/unju/fi/util/FechaUtil.java
@@ -4,13 +4,28 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Clase utilitaria para operaciones relacionadas con fechas.
+ * 
+ * Proporciona métodos para manejar fechas y cálculos relacionados con la edad.
+ */
 public class FechaUtil {
-	
-	public int calcularEdad(String fechaNacimientoStr) {
-	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-	    LocalDate fechaNacimiento = LocalDate.parse(fechaNacimientoStr, formatter);
-	    LocalDate ahora = LocalDate.now();
-	    return Period.between(fechaNacimiento, ahora).getYears();
-	}
+
+    /**
+     * Calcula la edad en años a partir de una fecha de nacimiento.
+     * 
+     * @param fechaNacimientoStr La fecha de nacimiento en formato "dd-MM-yyyy".
+     * @return La edad en años calculada desde la fecha de nacimiento hasta la fecha actual.
+     */
+    public int calcularEdad(String fechaNacimientoStr) {
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+                
+        LocalDate fechaNacimiento = LocalDate.parse(fechaNacimientoStr, formatter);
+                
+        LocalDate ahora = LocalDate.now();
+                
+        return Period.between(fechaNacimiento, ahora).getYears();
+    }
 
 }
--- tallerTp03/src/ar/edu/unju/fi/util/Totales.java
+++ tallerTp03/src/ar/edu/unju/fi/util/Totales.java
@@ -1,40 +1,52 @@
 package ar.edu.unju.fi.util;
 
+/**
+ * Clase que representa los totales de cálculo de sueldo para un empleado.
+ * 
+ * Esta clase almacena y proporciona métodos para acceder a los totales de remunerativos, salario familiar, descuentos y sueldo neto.
+ */
 public class Totales {
-	
-	private double remunerativos;
+
+    private double remunerativos;
     private double salarioFamiliar;
     private double descuentos;
     private double sueldoNeto;
 
-    // Métodos getter y setter
+
     public double getRemunerativos() {
         return remunerativos;
     }
+
 
     public void setRemunerativos(double remunerativos) {
         this.remunerativos = remunerativos;
     }
 
+
     public double getSalarioFamiliar() {
         return salarioFamiliar;
     }
+
 
     public void setSalarioFamiliar(double salarioFamiliar) {
         this.salarioFamiliar = salarioFamiliar;
     }
 
+
     public double getDescuentos() {
         return descuentos;
     }
+
 
     public void setDescuentos(double descuentos) {
         this.descuentos = descuentos;
     }
 
+
     public double getSueldoNeto() {
         return sueldoNeto;
     }
+
 
     public void setSueldoNeto(double sueldoNeto) {
         this.sueldoNeto = sueldoNeto;
